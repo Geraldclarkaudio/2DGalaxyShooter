74297c28c299b1cb086acdb0b5835b8f1a53afcb
jerbear666 <jerryclarkmusic@gmail.com>
2021-04-04 10:08:29 -0500

    Added Boundaries for player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +19,29 @@ public class Player : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        //transform.Translate(Vector3.right * horizontalInput  * _speed * Time.deltaTime);
+       // transform.Translate(Vector3.right * horizontalInput  * _speed * Time.deltaTime);
         //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        //if player position on y is greagter than 0, set y position to 0. 
+        if(transform.position.y >= 0)
+        {
+            //uses current position on x axis so it doesnt snap to the middle
+            transform.position = new Vector3(transform.position.x,0,0);
+        }
+        else if(transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if(transform.position.x >= 11.2f)
+        {
+            transform.position = new Vector3(-9.4f, transform.position.y, 0);
+        }
+        else if(transform.position.x <= -11.2f)
+        {
+            transform.position = new Vector3(9.4f, transform.position.y, 0);
+        }
     }
 }
