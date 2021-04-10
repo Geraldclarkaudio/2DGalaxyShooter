3acaaee51abf16001203247506f0f7a85b388f8e
jerbear666 <jerryclarkmusic@gmail.com>
2021-04-09 19:09:39 -0500

    Converted to 2D sprites

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -23,7 +23,7 @@ public class Enemy : MonoBehaviour
             transform.position = new Vector3(randomX, 7.5f, 0);
         }
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
