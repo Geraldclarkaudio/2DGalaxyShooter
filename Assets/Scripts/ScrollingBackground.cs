0a26c7f6ed2ecd6769748f1d7b8e8191154ea101
jerbear666 <jerryclarkmusic@gmail.com>
2021-07-11 00:20:14 -0500

    added scrolling Background

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 0.05f;
+
+    [SerializeField]
+    private GameObject backgroundPrefab;
+
+    [SerializeField]
+    private Vector3 originalPos;
+
+    [SerializeField]
+    private bool hasSpawned = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasSpawned = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed);
+
+        if(transform.position.y <= -4.4f && hasSpawned == false)
+        {
+            Debug.Log("Position should instantiate now");
+            Instantiate(backgroundPrefab, originalPos, Quaternion.identity);
+            hasSpawned = true;
+        }
+        if(transform.position.y <= -16.3f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
